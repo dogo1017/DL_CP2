c594e2dc6a389a6569a116ef0db929d835b21190
dogo1017 <douglas.london@ucas-edu.net>
2026-01-12 09:52:56 -0700

    financ calc and list notes

--- individual_projects/financial_calculator.py
+++ individual_projects/financial_calculator.py
@@ -1,35 +1,39 @@
 #DL 1st, financial calculator
 
+calcs = ["goal save"]
+
 # define stupid proofing function that takes in arguments of the required input, and datatype, and then using try and except to test if it is the right type
 
 def sp_input(type):
-    user_input = input()
-    if type == 'int':
-        try:
-            return(int(user_input))
-        except:
-            TypeError
-    if type == 'float':
-        try:
-            return(float(user_input))
-        except:
-            TypeError
-    if type == 'str':
-        try:
-            return(str(user_input))
-        except:
-            TypeError
+    while True:
+        user_input = input()
+        if type == 'int':
+            try:
+                return(int(user_input))
+            except TypeError:
+                print("That is not a valid input\n")
+                continue
+        if type == 'float':
+            try:
+                return(float(user_input))
+            except TypeError:
+                print("That is not a valid input\n")
+                continue
+        if type == 'str':
+            try:
+                return(str(user_input).strip().lower())
+            except TypeError:
+                print("That is not a valid input\n")
+                continue
     
 
-# define function starting_menu that will give the user a menu with options to open the different calculator functions
-
-def starting_menu(calcs):
-    counter = 0
-    for i in calcs:
-        counter += 1
-        print()
 
 # goal_save function, uses stupid proof function to take in what they are saving to, how often they are contibuting (weekly/monthly), how much you contribute each time, then define gs_calc func that divides the total amount by the amount you contribute, and then printing off the amount of weeks/months it will take
+def goal_save():
+    print("1) Monthly\n2) Weekly")
+    contribute_time = 0
+    while contribute_time != 1 or contribute_time != 2:
+        contribute_time = sp_input(int)
 
 # define compound_interest function that uses sp func to take in the starting amount, interest rate, years soent compounding, and then define the function ci_calc which takes the starting amount, multiplies the amount by the percent, adds it, and then loops through that process for as many years spent compounding
 
@@ -38,3 +42,33 @@ def starting_menu(calcs):
 # define sales_price function that uses sp to take in the origonal cost of the item, the precent of discount, and orunts out the origonal cost - the percentage.
 
 # define tip_calc that uses sp to take in how much the bill is, the precent of tip, and prints out the amount for tip and total after
+
+# define function starting_menu that will give the user a menu with options to open the different calculator function
+
+
+def starting_menu(calcs):
+    print("FINANCIAL CALCULATOR")
+    counter = 0
+    for i in calcs:
+        counter += 1
+        print(f"{counter}) {i}")
+    calc_choice = 0
+    while calc_choice <= 0 or calc_choice > counter: 
+        print(f"Which calculator would you like to use(1-{counter}):")
+        calc_choice = sp_input(str)
+        print("That is not a valid input")
+    if calc_choice == 1:
+        goal_save()
+    elif calc_choice == 2:
+        compund_interest()
+    elif calc_choice == 3:
+        budget_allocator()
+    elif calc_choice == 4:
+        sales_prince()
+    elif calc_choice == 5:
+        tip_calc()
+    else:
+        print("That is not a valid input")
+
+
+starting_menu(calcs)
--- notes/lists.py
+++ notes/lists.py
@@ -1,1 +1,23 @@
 #DL 1st, lists notes
+
+siblings = ["Bennett", "Rosalie", "Sarianne", "Brielle", "Sarianne"]
+
+print(siblings[3])
+siblings[-1] = "EGG"
+print(siblings)
+
+# tuple
+
+fruit = ("apple", "orange", "peach", "Kiwi", "raspberry")
+
+#set
+
+colors = {"Orange", "Purple", "Green", "Blue", "Yellow", "Red"}
+
+colors.add("Pink")
+colors.remove("Purple")
+
+for i in colors:
+    if i == "Orange":
+        print("fruit")
+    print(i)
