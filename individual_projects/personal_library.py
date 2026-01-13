dba72c5be729f4b0a88f052653635a07f1343e01
dogo1017 <douglas.london@ucas-edu.net>
2026-01-13 09:36:00 -0700

    start pseudocode personal lib

---
+++ individual_projects/personal_library.py
@@ -0,0 +1,11 @@
+#DL 1st, personal library 
+
+books = {}
+
+# define function sp_input, which takes in the arguments of wether there is a set answer, list of valid answers if there is, type that should be returned (int, str, float)
+
+# define view_lib function that print of all books and authors, and gives them the the option to return, or sort items by title or author alphabetically depending on which is not currently being sorted by
+
+
+# Define function main_menu which gives the options to 1. View, 2. Add, 3. Remove, 4 Search, 5. Exit, then uses sp_input to take in the input as a int with set awnsers as 1-5, depending on the awnser it will run the view_lib function, add_book function, remove_book function, search_lib function, and exit which will end the current main_menu function
+
