4637a9e5feae643e9ce85b44facf6abf99002551
dogo1017 <douglas.london@ucas-edu.net>
2026-02-05 12:18:12 -0700

    dino game cacti

--- extra/dino_game_folder/dino_game.py
+++ extra/dino_game_folder/dino_game.py
@@ -1,8 +1,9 @@
 import pygame
+import random
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 # Create a player rectangle (x, y, width, height)
-player = pygame.Rect(350, 500, 50, 50) 
+player = pygame.Rect(150, 500, 50, 50) 
 # Create an obstacle
 obstacle = pygame.Rect(900, 500, 50, 50)
 
@@ -13,15 +14,30 @@ image_rect = image_surface.get_rect()
 image_rect.center = (800 // 2, 600 // 2) # Position the image
 screen.blit(image_surface, image_rect)
 """
+obstacles = []
+
+def spawn_cactus():
+    count = random.randint(1, 3)    # Group size (1, 2, or 3 cacti)
+    height = random.choice([40, 60, 80])  # 3 different cactus heights
+    
+    for i in range(count):
+        # Spacing each cactus in the group by 30 pixels
+        new_cactus = pygame.Rect(900 + (i * 30), 550 - height, 30, height)
+        obstacles.append(new_cactus)
 
 velocity = 0
 running = True
 touching_ground = True
+spawn_cactus()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    obstacle.x -= 5
+    
+    for obstacle in obstacles:
+      obstacle.x -= 5
+    
+    
     
     keys = pygame.key.get_pressed()
     if touching_ground:
@@ -29,7 +45,7 @@ while running:
             velocity = -12  # Use = to assign, and negative to go UP
 
     player.y += velocity    # Apply velocity to the player's position
-
+ 
     # Collision Detection
     if player.y >= 500:
         player.y = 500
@@ -44,5 +60,10 @@ while running:
     pygame.draw.rect(screen, (255, 0, 0), obstacle) # Draw obstacle
     pygame.display.flip()
     clock.tick(60) # 60 FPS
+    if obstacle.x <= -50:
+      obstacle.x = 900
+    if player.colliderect(obstacle):
+        print("Game Over!")
+        running = False
 
 pygame.quit()
