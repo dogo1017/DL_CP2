3df95e2d1e3fa940ae99446d523731fdc751114b
dogo1017 <douglas.london@ucas-edu.net>
2026-02-09 09:51:56 -0700

    reading and writing csv and normal files

--- extra/tempCodeRunnerFile.py
+++ extra/tempCodeRunnerFile.py
@@ -2,3 +2,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression # A good starting model for text
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import wikipedia
+from word2number import w2n
+import requests
+from bs4 import BeautifulSoup
