836d6e2d2923fcc19e650d9cc4622aa5cd147e1c
dogo1017 <douglas.london@ucas-edu.net>
2026-02-27 21:22:03 -0700

    generating logic

--- individual_projects/fractal_generator/main.py
+++ individual_projects/fractal_generator/main.py
@@ -1,25 +1,30 @@
 import turtle
 
-start_chords = [[-100,0]]
+chords = [[-50,0],[50,0],[0,100]]
 
-def draw_fractal(turtle,distance,chords,recursions):
-    if recursions == 0: return
-    turtle.goto(chords[0])
-    chords.append([turtle.xcor(), turtle.ycor()])
-    turtle.forward(distance)
-    turtle.left(120)
-    chords.append([turtle.xcor(), turtle.ycor()])
-    turtle.forward(distance)
-    turtle.left(120)
-    chords.append([turtle.xcor(), turtle.ycor()])
-    turtle.forward(distance)
-    turtle.left(120)
-    chords.pop(0)
-    recursions -= 1
-    return draw_fractal(turtle,distance,chords,recursions)
+def midpt(pt1,pt2):
+  return ((pt1[0] + pt2[0])/2, ((pt1[1] + pt2[1]) /2))
+  
 
+def ind_triangle(points, turtle):
+  turtle.up()
+  turtle.goto(points[0])
+  turtle.down()
+  turtle.goto(points[1])
+  turtle.goto(points[2])
+  turtle.goto(points[0])
+
+def draw_fractal(turtle,chords,recursion):
+  ind_triangle(chords,turtle)
+  
+  if recursion > 0:
+    draw_fractal(turtle,[chords[0],midpt(chords[0],chords[1]),midpt(chords[0],chords[2])],recursion-1)
+    draw_fractal(turtle,[chords[1],midpt(chords[0],chords[1]),midpt(chords[1],chords[2])],recursion-1)
+    draw_fractal(turtle,[chords[2],midpt(chords[2],chords[1]),midpt(chords[0],chords[2])],recursion-1)
+  
+    
 
 draw = turtle.Turtle()
 draw.speed(0)
-draw_fractal(draw,100,start_chords,5)
+draw_fractal(draw,chords,7)
 turtle.done()
