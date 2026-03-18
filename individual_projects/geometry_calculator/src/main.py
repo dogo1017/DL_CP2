04f678f5efaea7e8dda8dee6825e9c29d5ec5fa2
dogo1017 <douglas.london@ucas-edu.net>
2026-03-18 20:38:08 +0000

    geometry cacl thng

--- individual_projects/geometry_calculator/src/main.py
+++ individual_projects/geometry_calculator/src/main.py
@@ -2,11 +2,17 @@
 
 import shapes
 
+try:
+    shape_count = 0
+    with open("individual_projects/geometry_calculator/docs/shapes.csv", 'r') as f:
+        shapes = [line.strip() for line in f if line.strip()]
+        line_count = len(shapes)
+            
+except:
+    line_count = 0
+    with open("individual_projects/geometry_calculator/docs/shapes.csv", 'w') as f:
+        pass
 def main():
-
-    with open("individual_projects\geometry_calculator\docs\shapes.csv", 'r') as f:
-        line_count = sum(1 for line in f)
-        
     print('='*38)
     print("📐 GEOMETRY CALCULATOR 📐")
     print('='*38)
@@ -15,5 +21,9 @@ def main():
     print("🔷 MAIN MENU 🔷")
     print('='*38)
     print(f"Current Shapes: {line_count} created")
+    print("┌─────────────────────────────────────┐")
+    for i in range(line_count):
+        print(f"| {shapes[i]}" + " "*(36-len(shapes[i])) + "|")
+    print("└─────────────────────────────────────┘")
 
 main()
